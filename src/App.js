c3c8e2fd2881c2f1573cda783f49bfa0afb9d1ae
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-19 19:54:23 +0600

    load api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+       <MainCountry></MainCountry>
     </div>
   );
+}
+
+function MainCountry(){
+    const [country, setcountry] = useState([]);
+
+    useEffect(()=> {
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setcountry(data))
+    },[])
+
+    return(
+    <div>
+      <h2>Name : </h2>
+      <h3>Capital: </h3>
+      <h4>Population: </h4>
+    </div>
+  )
+}
+
+function Country(){
+  return(
+    <div>
+      <h2>Name : </h2>
+      <h3>Capital: </h3>
+      <h4>Population: </h4>
+    </div>
+  )
 }
 
 export default App;
